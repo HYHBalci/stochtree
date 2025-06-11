eaaf8f7bcc1219881780da0919f9e855abbddfcd
HYHBalci <yusufhanbalci@gmail.com>
2025-06-11 11:04:35 +0200

    Horseshoe sampling update,for new data structure.

--- src/horseshoe_samplers.hpp
+++ src/horseshoe_samplers.hpp
@@ -34,10 +34,30 @@ double sample_alpha_cpp(int N, cpp11::writable::doubles r_alpha,
 double sample_sigma2_ig_cpp(int N, cpp11::writable::doubles resid,
                             double shape_prior, double rate_prior);
 
-cpp11::writable::list updateLinearTreatmentCpp_cpp(
-    cpp11::doubles_matrix<> X, cpp11::doubles Z, cpp11::writable::doubles residual,
-    double alpha, cpp11::writable::doubles beta, cpp11::writable::doubles beta_int,
-    cpp11::writable::doubles tau_beta, double tau_int, double sigma, double alpha_prior_sd, double tau_glob = 1, 
-    bool global_shrink = false, bool unlink = false);
+cpp11::writable::doubles updateLinearTreatmentCpp_cpp(
+    cpp11::doubles_matrix<> X,
+    cpp11::doubles Z,    
+    cpp11::doubles propensity_train,
+    cpp11::writable::doubles residual,
+    double alpha,
+    cpp11::writable::doubles beta,  
+    double gamma,
+    cpp11::writable::doubles beta_int,
+    cpp11::writable::doubles tau_beta,  
+    cpp11::writable::doubles nu,
+    double xi,
+    double tau_int,
+    double sigma,
+    double alpha_prior_sd,
+    double tau_glob = 1.0,
+    bool global_shrink = false,
+    bool unlink = false,
+    bool propensity_seperate = false,
+    bool gibbs = false,
+    bool save_output = true,
+    int index = 1,
+    int max_steps = 50,
+    double step_out = 0.5
+);
 
 #endif 
